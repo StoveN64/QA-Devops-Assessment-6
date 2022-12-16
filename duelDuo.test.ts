20bcfd7ded21e4dd7ff93f241e74861a2c8a2d9b
Steven Nersesian <sn3541@gmail.com>
2022-12-16 12:36:12 -0700

    Second Commit, fixed GitHub Issues

--- duelDuo.test.ts
+++ duelDuo.test.ts
@@ -18,3 +18,25 @@ test('Title shows up when page loads', async () => {
     const displayed = await title.isDisplayed()
     expect(displayed).toBe(true)
 })
+
+test('Draw button displays', async () => {
+    const drawButton = await driver.findElement(By.id('draw'))
+    const displayed = await drawButton.isDisplayed()
+    expect(displayed).toBe(true)
+})
+
+test('Div of choices is displayed', async () => {
+    const clickButton = await driver.findElement(By.id('draw')).click()
+    const change = await driver.findElement(By.id('choices'))
+    const displayed = await change.isDisplayed()
+    expect(displayed).toBe(true)
+})
+
+test('Clicking "add to duo" button shows the div with the id of "player-duo"', async () => {
+    await driver.findElement(By.id('draw')).click()
+    await driver.sleep(100)
+    await driver.findElement(By.css('.bot-btn')).click()
+    let playerDuoSection = await driver.findElement(By.id('player-duo'))
+    let displayed = await playerDuoSection.isDisplayed()
+    expect(displayed).toBe(true)
+})
--- functions.test.ts
+++ functions.test.ts
@@ -1,5 +1,13 @@
 const {shuffleArray} = require('./utils')
 
+let testArr = ['one', 'two', 'three']
+let resultArr = shuffleArray(testArr)
+
 describe('shuffleArray should', () => {
-    // CODE HERE
+    test('return an array', () => {
+        expect(Array.isArray(resultArr)).toBe(true)
+    })
+    test('scramble an array', () => {
+        expect(resultArr).not.toEqual(testArr)
+    })
 })
